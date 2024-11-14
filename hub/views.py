43a552e93076eb6aa143dc07729b026cb8274bfe
taslimaakter <22101186@uap-bd.edu>
2024-11-15 01:53:06 +0600

    taslima's code

--- hub/views.py
+++ hub/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import render
+from .models import Market_place
 def Home (request):
     return render (request,'home.html')
 # Create your views here.
+
+def home(request):
+    art= Market_place.objects.all()
+    context= {
+        'art': art,
+    }
+
+    return render (request,'home.html',context=context)
